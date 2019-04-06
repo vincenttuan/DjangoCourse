22d1f5c86b0aa3d8ecf81ae8e15329f93266f264
vincentjava <vincentjava@yahoo.com.tw>
2019-04-06 16:25:24 +0800

    Django

--- DjangoCourse/form.py
+++ DjangoCourse/form.py
@@ -5,7 +5,8 @@ class PostFormModel_Profile(forms.Form):
     username = forms.CharField(max_length=10,  initial='')
     password = forms.CharField(max_length=10,  initial='', widget=forms.PasswordInput)
     age      = forms.IntegerField()
-    sex      = forms.CharField(max_length=10,  initial='', required=False, widget=forms.RadioSelect)
+    CHOICES = (('男', '男性',), ('女', '女性',))
+    sex      = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES)
     email    = forms.EmailField(max_length=50, initial='', required=False)
     order    = forms.CharField(max_length=10,  initial='', required=False, widget=forms.CheckboxInput)
 
